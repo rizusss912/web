e277e349ec626bfc58912319fe25c13e22951fb5
rizusss912 <71278690+rizusss912@users.noreply.github.com>
2020-09-16 15:54:06 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,61 @@
+var inpName = document.getElementById('inpName');
+var inpFirstName = document.getElementById('inpFirstName');
+var inpLogin = document.getElementById('inpLogin');
+var inpPasword = document.getElementById('password-input');
+var inpTwoPasword = document.getElementById('password-input2');
+var inpEmail = document.getElementById('inpEmail');
+
+  inpName.onfocus = function (event) {
+    document.getElementById('inpTextName').className = "textinput2";
+  };
+  inpName.onblur = function (event) {
+    if(inpName.value==""){
+    document.getElementById('inpTextName').className = "textinput";
+  }
+  };
+  inpFirstName.onfocus = function (event) {
+    document.getElementById('inpTextFirstName').className = "textinput2";
+  };
+  inpFirstName.onblur = function (event) {
+    if(inpFirstName.value==""){
+    document.getElementById('inpTextFirstName').className = "textinput";
+  }
+  };
+  inpLogin.onfocus = function (event) {
+    document.getElementById('inpTextLogin').className = "textinput2";
+  };
+  inpLogin.onblur = function (event) {
+    if(inpLogin.value==""){
+    document.getElementById('inpTextLogin').className = "textinput";
+  }
+  };
+  inpPasword.onfocus = function (event) {
+    document.getElementById('inpTextPasword').className = "textinput2";
+  };
+  inpPasword.onblur = function (event) {
+    if(inpPasword.value==""){
+    document.getElementById('inpTextPasword').className = "textinput";
+  }
+  };
+  inpTwoPasword.onfocus = function (event) {
+    document.getElementById('inpTextTwoPasword').className = "textinput2";
+  };
+  inpTwoPasword.onblur = function (event) {
+    if(inpTwoPasword.value==""){
+    document.getElementById('inpTextTwoPasword').className = "textinput";
+  }
+  };
+  inpEmail.onfocus = function (event) {
+    document.getElementById('inpTextEmail').className = "textinput2";
+  };
+  inpEmail.onblur = function (event) {
+    if(inpEmail.value==""){
+    document.getElementById('inpTextEmail').className = "textinput";
+  }
+  };
+
+
+
 function show_hide_password(target){
   var input = document.getElementById('password-input');
   var input2 = document.getElementById('password-input2');
