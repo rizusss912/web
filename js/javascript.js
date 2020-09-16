b4e22da01f3290c7a02bd658e2a06d32cc83b3a7
rizusss912 <71278690+rizusss912@users.noreply.github.com>
2020-09-16 17:08:29 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -7,62 +7,62 @@ var inpEmail = document.getElementById('inpEmail');
 
   inpName.onfocus = function (event) {
     document.getElementById('inpTextName').className = "textinput2";
-    document.getElementById('inpTextName').parentNode.style.margin = "0px auto auto auto";
+    document.getElementById('inpTextName').parentNode.style.margin = "0px auto 5px auto";
   };
   inpName.onblur = function (event) {
     if(inpName.value==""){
     document.getElementById('inpTextName').className = "textinput";
-    document.getElementById('inpTextName').parentNode.style.margin = "20px auto auto auto";
+    document.getElementById('inpTextName').parentNode.style.margin = "20px auto 5px auto";
   }
   };
   inpFirstName.onfocus = function (event) {
     document.getElementById('inpTextFirstName').className = "textinput2";
-    document.getElementById('inpTextFirstName').parentNode.style.margin = "0px auto auto auto";
+    document.getElementById('inpTextFirstName').parentNode.style.margin = "0px auto 5px auto";
   };
   inpFirstName.onblur = function (event) {
     if(inpFirstName.value==""){
     document.getElementById('inpTextFirstName').className = "textinput";
-    document.getElementById('inpTextFirstName').parentNode.style.margin = "20px auto auto auto";
+    document.getElementById('inpTextFirstName').parentNode.style.margin = "20px auto 5px auto";
   }
   };
   inpLogin.onfocus = function (event) {
     document.getElementById('inpTextLogin').className = "textinput2";
-    document.getElementById('inpTextLogin').parentNode.style.margin = "0px auto auto auto";
+    document.getElementById('inpTextLogin').parentNode.style.margin = "0px auto 5px auto";
   };
   inpLogin.onblur = function (event) {
     if(inpLogin.value==""){
     document.getElementById('inpTextLogin').className = "textinput";
-    document.getElementById('inpTextLogin').parentNode.style.margin = "20px auto auto auto";
+    document.getElementById('inpTextLogin').parentNode.style.margin = "20px auto 5px auto";
   }
   };
   inpPasword.onfocus = function (event) {
     document.getElementById('inpTextPasword').className = "textinput2";
-    document.getElementById('inpTextPasword').parentNode.style.margin = "0px auto auto auto";
+    document.getElementById('inpTextPasword').parentNode.style.margin = "0px auto 5px auto";
   };
   inpPasword.onblur = function (event) {
     if(inpPasword.value==""){
     document.getElementById('inpTextPasword').className = "textinput";
-    document.getElementById('inpTextPasword').parentNode.style.margin = "20px auto auto auto";
+    document.getElementById('inpTextPasword').parentNode.style.margin = "20px auto 5px auto";
   }
   };
   inpTwoPasword.onfocus = function (event) {
     document.getElementById('inpTextTwoPasword').className = "textinput2";
-    document.getElementById('inpTextTwoPasword').parentNode.style.margin = "0px auto auto auto";
+    document.getElementById('inpTextTwoPasword').parentNode.style.margin = "0px auto 5px auto";
   };
   inpTwoPasword.onblur = function (event) {
     if(inpTwoPasword.value==""){
     document.getElementById('inpTextTwoPasword').className = "textinput";
-    document.getElementById('inpTextTwoPasword').parentNode.style.margin = "20px auto auto auto";
+    document.getElementById('inpTextTwoPasword').parentNode.style.margin = "20px auto 5px auto";
   }
   };
   inpEmail.onfocus = function (event) {
     document.getElementById('inpTextEmail').className = "textinput2";
-    document.getElementById('inpTextEmail').parentNode.style.margin = "0px auto auto auto";
+    document.getElementById('inpTextEmail').parentNode.style.margin = "0px auto 5px auto";
   };
   inpEmail.onblur = function (event) {
     if(inpEmail.value==""){
     document.getElementById('inpTextEmail').className = "textinput";
-    document.getElementById('inpTextEmail').parentNode.style.margin = "20px auto auto auto";
+    document.getElementById('inpTextEmail').parentNode.style.margin = "20px auto 5px auto";
   }
   };
 
